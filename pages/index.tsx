15fe578fb4a38b9d8a01d298e0b6ecacb0d0d264
meisterjustice <eziefulejustice@gmail.com>
2023-02-20 01:59:59 +0400

    chore: configure tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
-    <>
-      <div>Home</div>
-    </>
+    <div className="flex flex-col justify-center items-center w-screen">
+      <div className="container">
+        <div className="flex justify-center items-center h-screen">
+          {' '}
+          <div className="text-2xl text-blue-500">Home</div>
+        </div>
+      </div>
+    </div>
   );
 }
