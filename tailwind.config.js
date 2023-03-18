a2a020fd79f57774357d32be1eee803af01c96d5
Favour Ifunanya <chocorazzee@gmail.com>
2023-03-19 00:20:54 +0100

    screen dimensions in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
     extend: {},
   },
   plugins: [],
